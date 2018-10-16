83659512b1f7ec55906321d3982da74fed33484c
Camden Penn <camden.penn@gmail.com>
2018-10-16 18:37:07 -0400

    Isolated drawing-canvas component; added brush tool.

--- Japanese-Drawing-Practice/src/app/app.component.ts
+++ Japanese-Drawing-Practice/src/app/app.component.ts
@@ -1,10 +1,206 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DrawingCanvasComponent } from './drawing-canvas/drawing-canvas.component';
+import * as Tesseract from 'tesseract.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Japanese Handwriting Practice';
+  
+  clear_canvas:boolean=false;
+  image_data:ImageData=new ImageData(1,1);
+
+  randomize:boolean=false;
+  checking_answer:boolean=false;
+  check_answer_progress:number=0;
+  checking_task:string="";
+  done:boolean = false;
+
+  debugMode:boolean = true;
+  myTesseract:Tesseract.TesseractStatic;
+  readonly all_hiragana_chars:string='あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだじづでどばびぶべぼぱぴぷぺぽゃゅょっ';
+  readonly all_katakana_chars:string='アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフへホマミムメモヤユヨラリルレロワヲンがギグゲゴザジズゼゾダヂヅデドバビブベボパピプぺポャュョッー';
+  readonly expected_kanji:string='零一ニ三四五六七八九十百千万円時分何大学年生日本語私土曜半英毎今月食行';
+  tesseract_settings=
+  {
+    lang: 'jpn',
+    //The kanji '一' overrides the katakana 'ー'. For katakana practice, it is thus best to not include kanji.
+    tessedit_char_whitelist: this.all_hiragana_chars+this.all_katakana_chars+this.expected_kanji,
+  };
+  result:string='';
+  correct:boolean = null;
+
+  questions:question[]=
+  [
+    new question('Japan',['日本','にほん'],'nihon'),
+    new question('college',['大学','だいがく'],'daigaku'),
+    new question('ice cream',['アイスクリーム'],'Use katakana'),
+    new question('I',['私','わたし'],'watashi'),
+    new question('one',['一','いち'],'ichi'),
+    new question('two o\' clock',['ニ時','にじ'],'niji'),
+    new question('three o\'clock',['三時','さんじ'],'sanji'),
+    new question('four',['四','よん','し'],'yon'),
+    new question('five',['五','ご'],'go'),
+    new question('six o\' clock',['六時','ろくじ'],'rokuji'),
+    new question('seven',['七','しち','なな'],'shichi'),
+    new question('eight',['八','はち'],'hachi'),
+    new question('nine',['九','きゅう','く'],'kyu'),
+    new question('ten',['十','じゅう'],'jyu'),
+    new question('hundred',['百','ひゃく'],'hyaku'),
+    new question('thousand',['千','せん'],'sen'),
+    new question('ten thousand',['万','まん'],'man'),
+    new question('yen',['円','えん'],'en'),
+  ]; //List of questions [TODO: init from file?]
+  question_order:number[]; //randomize this to shuffle questions
+  curr_q_index:number=0; //Index within question order currently being asked
+  curr_question:question; //Reference to the current question
+  hint_enabled:boolean = false;
+
+  ngOnInit():void {
+    //Import URL arguments.
+    let URL_divided=document.URL.split('?');
+    if(URL_divided.length>1){
+      let URL_args:string[] = URL_divided[1].split('&');
+      for(let i=0;i<URL_args.length;i++){
+        if (URL_args[i]=='randomize'){
+          this.randomize=true;
+        }
+      }
+    } 
+
+    //Fill question_order with 1..[number of questions].
+    this.question_order=[];
+    for(let i=0;i<this.questions.length;i++){
+      this.question_order.push(i);
+    }
+    //Shuffle if needed.
+    if(this.randomize){
+      this.shuffle();
+    }
+    this.update_question();
+    //Initialize Tesseract
+    this.myTesseract = Tesseract.create({
+      workerPath: 'https://cdn.rawgit.com/naptha/tesseract.js/1.0.10/dist/worker.js',
+      langPath: 'https://cdn.rawgit.com/naptha/tessdata/gh-pages/3.02/',
+      corePath: 'https://cdn.rawgit.com/naptha/tesseract.js-core/0.1.0/index.js',
+    });
+    this.check_answer(true);
+    if(this.debugMode){
+      console.log('Init done.')
+    }
+  }
+  shuffle():void{
+    //Shuffles the values in question_order.
+    let j:number, x:number, i:number;
+    for (i = this.question_order.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = this.question_order[i];
+        this.question_order[i] = this.question_order[j];
+        this.question_order[j] = x;
+    }
+  }
+  update_image(new_img:ImageData){
+    this.image_data=new_img;
+  }
+  clear_screen(){
+    if(this.debugMode){
+      console.log(this.clear_canvas)
+      console.log('Trying to clear screen.')
+    }
+    this.clear_canvas=true;
+    //setTimeout(function(){this.clear_canvas=false;console.log(this.clear_canvas);},10);
+  }
+  previous_question():void{
+    if(this.curr_q_index>0){
+      this.curr_q_index--;
+      this.update_question();
+    }
+  }
+  next_question():void{
+    if(this.curr_q_index<this.questions.length-1){
+      this.curr_q_index++;
+      this.update_question();
+    }else{
+      this.done = true;
+    }
+  }
+  update_question():void{
+    this.curr_question = this.questions[this.question_order[this.curr_q_index]]; 
+    this.hint_enabled = false;
+    this.result='';
+    this.correct=null;
+    this.clear_screen();
+  }
+  toggle_show_hint():void{
+    this.hint_enabled=!this.hint_enabled;
+  }
+  get_hint_button_color():string{
+    if(this.hint_enabled){
+      return 'primary';
+    }else{
+      return '';
+    }
+  }
+  check_answer(init:boolean=false):void{
+    if(this.debugMode){
+      console.log('Checking.')
+    }
+    //TODO: feed image from canvas into Tesseract; compare result to expected answer
+    if(init){
+      this.myTesseract.recognize(this.image_data, 
+          this.tesseract_settings
+        )
+        .progress(packet => console.info(packet))
+        .then(data => console.log(data))
+        .finally(data => console.log(data))
+    }else{
+      this.checking_answer=true;
+      this.myTesseract.recognize(this.image_data, 
+        this.tesseract_settings
+      )
+      .progress(packet => this.update_progress_bar(packet))
+      .then(data => this.apply_result(data.text))
+    }
+  }
+  update_progress_bar(packet):void{
+    this.checking_task = packet.status;
+    this.check_answer_progress = packet.progress*100;
+  }
+  apply_result(text:string):void{
+    this.result=this.clear_whitespace_from(text);
+    this.correct = this.curr_question.check_answer(this.result);
+    this.checking_answer=false;
+  }
+  clear_whitespace_from(text:string):string{
+    return text.replace(/\s/g,'').trim();
+  }
+  restart():void{
+    if(this.randomize){
+      this.shuffle();
+    }
+    this.curr_q_index=0;
+    this.update_question();
+    this.done=false;
+  }
+}
+class question{
+  question:string='';
+  answers:string[]=[''];
+  hint:string="";
+  constructor(question:string,answers:string[],hint:string){
+    this.question=question;
+    this.answers=answers;
+    this.hint=hint;
+  }
+  check_answer(potential_ans:string):boolean{
+    let to_return:boolean=false;
+    for(let curr_index:number=0;!to_return && curr_index < this.answers.length; curr_index++){
+      if(this.answers[curr_index]==potential_ans){
+        to_return = true;
+      }
+    }
+    return to_return;
+  }
 }
--- Japanese-Drawing-Practice/src/app/drawing-canvas/drawing-canvas.component.ts
+++ Japanese-Drawing-Practice/src/app/drawing-canvas/drawing-canvas.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
-import * as Tesseract from 'tesseract.js';
+import { Component, ViewChild, ElementRef, EventEmitter, HostListener, Input, Output, OnChanges, SimpleChange } from '@angular/core';
+
 
 @Component({
   selector: 'drawing-canvas',
@@ -7,21 +7,26 @@ import * as Tesseract from 'tesseract.js';
   styleUrls: ['./drawing-canvas.component.css']
 })
 
-export class DrawingCanvasComponent implements OnInit {
-  
-  randomize:boolean=false;
-  checking_answer:boolean=false;
-  check_answer_progress:number=0;
-  checking_task:string="";
-  done:boolean = false;
-
-  debugMode:boolean = false;
+export class DrawingCanvasComponent implements OnChanges {
+  debugMode:boolean=false;
+  canvas_ready:boolean=false;
+  @Input('width') canvas_width:number = 500;
+  @Input('height') canvas_height:number = 200;
+  @Input('clear') request_clear_screen:boolean = false;
+  @Output('clearChange')screen_cleared = new EventEmitter<boolean>();
+  ngOnChanges(){
+    if(this.request_clear_screen){
+      this.clear_screen(true);
+      this.screen_cleared.emit(false);
+    }
+    this.get_new_canvas_position();
+  }
+  @Output()image_drawn = new EventEmitter<ImageData>();
 
   @ViewChild('drawing_canvas')canvas_elem:ElementRef<HTMLCanvasElement>;
   context:CanvasRenderingContext2D;
   offset_left:number=0;
   offset_top:number=0;
-
   @HostListener('window:resize')on_resize(){
     this.offset_left = this.canvas_elem.nativeElement.offsetLeft;
     this.offset_top = this.canvas_elem.nativeElement.offsetTop;
@@ -31,100 +36,24 @@ export class DrawingCanvasComponent implements OnInit {
   tool_in_use:tools=tools.Pencil; //Which tool is currently selected.
   points:point[]=[]; //The points to draw during a redraw.
   
-  myTesseract:Tesseract.TesseractStatic;
-  all_hiragana_chars:string='あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだじづでどばびぶべぼぱぴぷぺぽゃゅょっ';
-  all_katakana_chars:string='アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフへホマミムメモヤユヨラリルレロワヲンがギグゲゴザジズゼゾダヂヅデドバビブベボパピプぺポャュョッー';
-  expected_kanji:string='零一ニ三四五六七八九十百千万円時分何大学年生日本語私土曜半英毎今月食行';
-  tesseract_settings=
-  {
-    lang: 'jpn',
-    textord_min_xheight: 5,
-    tessedit_char_whitelist: /*this.all_hiragana_chars+*/this.all_katakana_chars/*+this.expected_kanji*/,
-  };
-
-  result:string='';
-  correct:boolean = null;
-
-  questions:question[]=
-  [
-    new question('Japan',['日本','にほん'],'nihon'),
-    new question('college',['大学','だいがく'],'daigaku'),
-    new question('ice cream',['アイスクリーム'],'Use katakana'),
-    new question('one',['一','いち'],'ichi'),
-    new question('two o\' clock',['ニ時','にじ'],'niji'),
-    new question('three o\'clock',['三時','さんじ'],'sanji'),
-    new question('four',['四','よん','し'],'yon'),
-    new question('five',['五','ご'],'go'),
-    new question('six',['六','ろく'],'roku'),
-    new question('seven',['七','しち','なな'],'shichi'),
-    new question('eight',['八','はち'],'hachi'),
-    new question('nine',['九','きゅう','く'],'kyu'),
-    new question('ten',['十','じゅう'],'jyu'),
-    new question('hundred',['百','ひゃく'],'hyaku'),
-    new question('thousand',['千','せん'],'sen'),
-    new question('ten thousand',['万','まん'],'man'),
-    new question('yen',['円','えん'],'en'),
-  ]; //List of questions [TODO: init from file?]
-  question_order:number[]; //randomize this to shuffle questions
-  curr_q_index:number=0; //Index within question order currently being asked
-  curr_question:question; //Reference to the current question
-  hint_enabled:boolean = false;
   constructor() { }
 
-  ngOnInit():void {
-    //Import URL arguments.
-    let URL_divided=document.URL.split('?');
-    if(URL_divided.length>1){
-      let URL_args:string[] = URL_divided[1].split('&');
-      for(let i=0;i<URL_args.length;i++){
-        if (URL_args[i]=='randomize'){
-          this.randomize=true;
-        }
-      }
-    } 
-
-    //Fill question_order with 1..[number of questions].
-    this.question_order=[];
-    for(let i=0;i<this.questions.length;i++){
-      this.question_order.push(i);
-    }
-    //Shuffle if needed.
-    if(this.randomize){
-      this.shuffle();
-    }
-  }
-  shuffle():void{
-    //Shuffles the values in question_order.
-    let j:number, x:number, i:number;
-    for (i = this.question_order.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = this.question_order[i];
-        this.question_order[i] = this.question_order[j];
-        this.question_order[j] = x;
-    }
-  }
+  
   ngAfterViewInit():void{
     
     //Initialize canvas
     this.context = this.canvas_elem.nativeElement.getContext('2d');
-    let el:HTMLElement=this.canvas_elem.nativeElement;
-    this.offset_left = el.offsetLeft;
-    this.offset_top = el.offsetTop;
+    this.get_new_canvas_position();
     this.clear_screen();
-    
-    this.update_question();
-    //Initialize Tesseract
-    this.myTesseract = Tesseract.create({
-      workerPath: 'https://cdn.rawgit.com/naptha/tesseract.js/1.0.10/dist/worker.js',
-      langPath: 'https://cdn.rawgit.com/naptha/tessdata/gh-pages/3.02/',
-      corePath: 'https://cdn.rawgit.com/naptha/tesseract.js-core/0.1.0/index.js',
-    });
-    this.check_answer(true);
-    if(this.debugMode){
-      console.log('Init done.')
+    this.canvas_ready=true;
+  }
+  get_new_canvas_position(){
+    if(this.canvas_ready){
+      let el:HTMLElement=this.canvas_elem.nativeElement;
+      this.offset_left = el.offsetLeft;
+      this.offset_top = el.offsetTop;
     }
   }
-
   begin_line(e:MouseEvent):void{
     if(this.debugMode){
       console.log('Click!')
@@ -147,6 +76,7 @@ export class DrawingCanvasComponent implements OnInit {
     if(this.debugMode){
       console.log('Adding location at (' + x + ', ' + y + ').')
     }
+    this.screen_cleared.emit(false);
     this.points.push(new point(x,y,is_dragging,this.tool_in_use));
     this.redraw_canvas();
   }
@@ -163,8 +93,17 @@ export class DrawingCanvasComponent implements OnInit {
           this.context.lineWidth = 3;
           break;
         }
+        case tools.Brush:{
+          this.context.strokeStyle="black";
+          if(!this.points[i].is_dragging){
+            this.context.lineWidth = 1.01;
+          }else{
+            this.context.lineWidth = this.context.lineWidth + ((this.context.lineWidth-1)*(4 - this.context.lineWidth)*0.3);
+          }
+          break;
+        }
         case tools.Eraser:{
-          this.context.strokeStyle = "#FFFFFF"; //white
+          this.context.strokeStyle = "white"; 
           this.context.lineWidth = 5;
           break;
         }
@@ -182,12 +121,10 @@ export class DrawingCanvasComponent implements OnInit {
       this.context.closePath();
       this.context.stroke();
     }
+    this.image_drawn.emit(this.context.getImageData(0,0,this.canvas_width,this.canvas_height));
   }
-  pencil_mode():void{
-    this.tool_in_use = tools.Pencil;
-  }
-  eraser_mode():void{
-    this.tool_in_use = tools.Eraser;
+  select_tool(tool:string):void{
+    this.tool_in_use = tools[tool];
   }
   tool_button_color(tool_type:string):string{
     let tool=tools[tool_type];
@@ -198,87 +135,19 @@ export class DrawingCanvasComponent implements OnInit {
     }
   }
   clear_screen(delete_points:boolean=false):void{
-    this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height); // Clears the canvas
-    this.context.fillStyle = "#FFFFFF";
-    this.context.fillRect(0,0,this.context.canvas.width, this.context.canvas.height);
-    if(delete_points){
-      this.points=[];
-      this.tool_in_use=tools.Pencil;
+    if(this.canvas_ready){
+      this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height); // Clears the canvas
+      this.context.fillStyle = "#FFFFFF";
+      this.context.fillRect(0,0,this.context.canvas.width, this.context.canvas.height);
+      if(delete_points){
+        this.points=[];
+        if(this.tool_in_use == tools.Eraser){
+          this.tool_in_use=tools.Pencil;
+        }
+      }
     }
   }
-  previous_question():void{
-    if(this.curr_q_index>0){
-      this.curr_q_index--;
-      this.update_question();
-    }
-  }
-  next_question():void{
-    if(this.curr_q_index<this.questions.length-1){
-      this.curr_q_index++;
-      this.update_question();
-    }else{
-      this.done = true;
-    }
-  }
-  update_question():void{
-    this.curr_question = this.questions[this.question_order[this.curr_q_index]]; 
-    this.hint_enabled = false;
-    this.points=[];
-    this.result='';
-    this.correct=null;
-    this.clear_screen(true);
-  }
-  toggle_show_hint():void{
-    this.hint_enabled=!this.hint_enabled;
-  }
-  get_hint_button_color():string{
-    if(this.hint_enabled){
-      return 'primary';
-    }else{
-      return '';
-    }
-  }
-  check_answer(init:boolean=false):void{
-    if(this.debugMode){
-      console.log('Checking.')
-    }
-    //TODO: feed image from canvas into Tesseract; compare result to expected answer
-    if(init){
-      this.myTesseract.recognize(this.context, 
-          this.tesseract_settings
-        )
-        .progress(packet => console.info(packet))
-        .then(data => console.log(data))
-        .finally(data => console.log(data))
-    }else{
-      this.checking_answer=true;
-      this.myTesseract.recognize(this.context, 
-        this.tesseract_settings
-      )
-      .progress(packet => this.update_progress_bar(packet))
-      .then(data => this.apply_result(data.text))
-    }
-  }
-  update_progress_bar(packet):void{
-    this.checking_task = packet.status;
-    this.check_answer_progress = packet.progress*100;
-  }
-  apply_result(text:string):void{
-    this.result=this.clear_whitespace_from(text);
-    this.correct = this.curr_question.check_answer(this.result);
-    this.checking_answer=false;
-  }
-  clear_whitespace_from(text:string):string{
-    return text.replace(/\s/,'').trim();
-  }
-  restart():void{
-    if(this.randomize){
-      this.shuffle();
-    }
-    this.curr_q_index=0;
-    this.update_question();
-    this.done=false;
-  }
+  
 }
 class point{
   x:number=0;
@@ -294,24 +163,6 @@ class point{
 }
 enum tools{
   Pencil = 0,
-  Eraser = 1,
-}
-class question{
-  question:string='';
-  answers:string[]=[''];
-  hint:string="";
-  constructor(question:string,answers:string[],hint:string){
-    this.question=question;
-    this.answers=answers;
-    this.hint=hint;
-  }
-  check_answer(potential_ans:string):boolean{
-    let to_return:boolean=false;
-    for(let curr_index:number=0;!to_return && curr_index < this.answers.length; curr_index++){
-      if(this.answers[curr_index]==potential_ans){
-        to_return = true;
-      }
-    }
-    return to_return;
-  }
+  Brush = 1,
+  Eraser = 2,
 }
